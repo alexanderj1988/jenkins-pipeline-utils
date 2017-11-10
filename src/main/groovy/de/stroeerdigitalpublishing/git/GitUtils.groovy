0ac5fc3c20fa93f8e805ae641e26ce6b8c08781f
Alexander Jandousek <alexander.jandousek@stroer.de>
2017-11-10 14:30:44 +0100

    added library class from private repo and added publishing method to bintray

---
+++ src/main/groovy/de/stroeerdigitalpublishing/git/GitUtils.groovy
@@ -0,0 +1,22 @@
+
+package de.stroeerdigitalpublishing.git
+
+class GitUtils {
+    def steps
+
+    GitUtils(steps) {
+        this.steps = steps
+    }
+
+    String getRepoName() {
+        return ((String)steps.scm.getUserRemoteConfigs()[0].getUrl().tokenize('/')[3].split("\\.")[0]).toLowerCase()
+    }
+
+    String getShortCommitHash () {
+        return steps.sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
+    }
+
+    String getCommitHash(){
+        return ((String) steps.sh(returnStdout: true, script: 'git rev-parse HEAD')).replace('\n','').replace('\r','').replace(' ','')
+    }
+}
